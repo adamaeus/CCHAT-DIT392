acbe23062b795cea74da93aa5d1a4e3ceee1e6e3
Adam Mos <moztrom93@gmail.com>
2023-10-09 16:08:52 +0200

    Server file working. Using Existing genserver.

--- server.erl
+++ server.erl
@@ -1,21 +1,25 @@
 -module(server).
--export([start/1,stop/1]).
+-export([start/1,stop/1,status/1]).
 
 % Start a new server process with the given name
 % Do not change the signature of this function.
 start(ServerAtom) ->
-    genserver:start(ServerAtom, State, F).
-    % TODO Implement function
-    % - Spawn a new process which waits for a message, handles it, then loops infinitely
-    % - Register this process to ServerAtom
-    % - Return the process ID
-    not_implemented.
+    genserver:start(ServerAtom, 0, fun server_handler/2).
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
-stop(ServerAtom) ->
-    ServerAtom ! {stop, self(), 0},
-    ok.
-    % TODO Implement function
-    % Return ok
-    % not_implemented.
+stop(ServerAtom) -> 
+    genserver:stop(ServerAtom).
+
+% Calls for the state of the server.
+status(ServerAtom) -> genserver:request(ServerAtom, status).
+
+%%% Main Module Handler %%%
+server_handler(State, Request) ->
+    case Request of
+        status -> status_handler(State, status)
+end.
+
+
+%%% Handlers %%%
+status_handler(State, status) -> {reply, State, State}.
