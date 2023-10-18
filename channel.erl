01581b2a4b9406c455c835f1ba145aa2bf845294
Adam Mos <moztrom93@gmail.com>
2023-10-18 10:12:40 +0200

    Update

--- channel.erl
+++ channel.erl
@@ -13,21 +13,13 @@ initial_state(Channel) ->
     }.
 
 start(Channel) ->
-    io:fwrite("~p ~n", [inside_channel_start]),
-    io:fwrite("~p ~n", [Channel]),
     State = initial_state(Channel),
-    io:fwrite("~p ~n", [after_STATE_inside_channel_start]),
-    io:fwrite("~p ~n", [State]),
-    genserver:start(list_to_atom(Channel), State, fun channel_handler/2),
-    io:fwrite("~p ~n", [after_genserver_start_in_channel]),
-    io:fwrite("~p ~n", [list_to_atom(Channel)]).
-    
+    genserver:start(list_to_atom(Channel), State, fun channel_handler/2).
 
 channel_handler(State, Request) ->
     case Request of
-        {join, Nick, From} ->
-            addToChannel(State, Nick, From),
-            {reply, user_added, #channel_state.clients}
+        {join, Nick, From} -> addToChannel(State, Nick, From),
+            {reply, user_added, State#channel_state.clients} %Added State
     end.
 
 
--- server.erl
+++ server.erl
@@ -1,7 +1,6 @@
 -module(server).
 -export([start/1, stop/1, chat_handler/2]).
 
-
 -record(server_state, {
     channels
 }).
@@ -10,70 +9,53 @@
 % Return an initial state record. This is called from GUI.
 % Do not change the signature of this function.
 
-
 initial_state() ->
     #server_state{
         channels = []
     }.
 
-
 % Logic server
 % Parameters match the generic server start function
 % Start a new server process with the given name
 % Do not change the signature of this function.
- % ServerAtom is "shire" in our case.
+% ServerAtom is "shire" in our case.
 start(ServerAtom) ->
     State = initial_state(),
-    genserver:start(ServerAtom, State, fun chat_handler/2),
-    io:format("module server, start function: ~p, serverAtom: ~p, State: ~p~n",  [#server_state.channels,ServerAtom, State]).
+    genserver:start(ServerAtom, State, fun chat_handler/2).
 
 %   - takes 2 params : state, request message
 %   - returns a tuple: new state, response message
 %   - handler reply to genserver
 chat_handler(State, Data) ->
     case Data of
-       {join, From, Channel, Nick} ->
-        io:format("chat handler: Channel: ~p, From: ~p, Nick: ~p, State: ~p~n", [Channel, From, Nick, State]),
+        {join, From, Channel, Nick} ->
             join_server(State, Channel, From, Nick)
-        end.
+    end.
 
 % Check to see if channel exist or not
 % If exist -> join that channel
 % If not exist -> Create and join
 join_server(State, Channel, From, Nick) ->
-    %Channels = State#server_state.channels,
     Channels = State#server_state.channels,
-    io:fwrite("~p ~n", [Channel]),
-    io:fwrite("~p ~n", [Channels]),
     case lists:member(Channel, Channels) of
         true ->
-            io:fwrite("true"),
-            %channel:join(Channel, Nick, From),
-            genserver:request(list_to_atom(Channel), {join, Nick, From});
+            io:fwrite("true");
         false ->
-            io:fwrite("~p ~n", [false]),
-            channel:start(Channel),
-            io:fwrite("~p ~n", [false_after_channel_start])
-                end,
-                
-                ChannelResponse = genserver:request(list_to_atom(Channel), {join, Nick, From}),
-                NewChannels = [Channel | Channels],
-                #server_state{channels = NewChannels},
-                {reply, ChannelResponse, State#server_state{channels=NewChannels}}.
+            channel:start(Channel)
+    end,
 
-
-
-    
-
-
-
+    ChannelResponse = genserver:request(list_to_atom(Channel), {join, Nick, From}),
+    NewChannels = [Channel | Channels],
+    State#server_state{channels = NewChannels},
+    {reply, ChannelResponse, State}.
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
 stop(ServerAtom) ->
-    genserver:stop(ServerAtom), % Added
+    % Added
+    genserver:stop(ServerAtom),
     ServerAtom ! {stop, self(), 0},
     ok.
-    % TODO Implement function
+% TODO Implement function
     % Return ok
     % not_implemented.
